5d7d36c2fbfd485b5bd162cb7d44f61f9fec5d5f
Jason Francis <jkfranci@iu.edu>
2017-10-14 01:33:27 -0400

    Early work of adapting the example plugin to read datat from BitChute.

---
+++ main.py
@@ -0,0 +1,272 @@
+# -*- coding: utf-8 -*-
+# Module: default
+# Author: Roman V. M.
+# Created on: 28.11.2014
+# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
+
+import sys
+from urlparse import parse_qsl
+import xbmcgui
+import xbmcplugin
+import re
+import requests
+from bs4 import BeautifulSoup
+
+# Get the plugin url in plugin:// notation.
+_url = sys.argv[0]
+# Get the plugin handle as an integer number.
+_handle = int(sys.argv[1])
+
+baseUrl = "https://www.bitchute.com"
+subscriptions = ["InRangeTV", "mediamonarchy"]
+class VideoLink:
+	def __init__(self, containerSoup):
+		titleDiv = containerSoup.findAll('div', "channel-videos-title")[0]
+		linkSoup = titleDiv.findAll('a')[0]
+		
+		self.title = linkSoup.string
+		self.pageUrl = linkSoup.get("href")
+		self.id = self.pageUrl.split("/")[-1]
+		self.thumbnail = None
+		self.url = None
+		#before we can find thumnails let's strip out play button images.
+		for playButton in containerSoup.findAll('img', "play-overlay-icon"):
+			playButton.extract()
+		
+		thumbnailMatches = containerSoup.findAll('img', "img-responsive")
+		
+		if thumbnailMatches:
+			self.thumbnail = baseUrl + thumbnailMatches[0].get("src")
+
+	def getUrl(self, channelId):
+		return(baseUrl + "/torrent/" + channelId + "/" + self.id + ".torrent")
+	def setUrl(self, channelId):
+		self.url = self.getUrl(channelId)
+
+class Channel:
+	def __init__(self, channelName):
+		self.channelName = channelName
+		self.videos = []
+		self.thumbnail = None
+
+		r = requests.get(baseUrl + "/" + self.channelName)
+		soup = BeautifulSoup(r.text, 'html.parser')
+
+		thumbnailImages = soup.findAll("img", id="fileupload-medium-icon-2")
+		if thumbnailImages:
+			self.thumbnail = baseUrl + thumbnailImages[0].get("src")
+
+		for videoContainer in soup.findAll('div', "channel-videos-container"):
+			self.videos.append(VideoLink(videoContainer))
+
+		# for now I only know how to find the ID from a video, so take the last item
+		# in videos and find the channel's ID.
+		videoRequest = requests.get(baseUrl + self.videos[-1].pageUrl)
+		channelIdMatches = re.search('/torrent/\d+', videoRequest.text)
+		if channelIdMatches:
+			self.id = channelIdMatches.group().split("/")[-1]
+		else:
+			raise ValueError("channel Id not found for " + self.channelName + ".")
+		
+		# armed with a channelId we can set the url for all our videos.
+		for video in self.videos:
+			video.setUrl(self.id)
+
+
+# Free sample videos are provided by www.vidsplay.com
+# Here we use a fixed set of properties simply for demonstrating purposes
+# In a "real life" plugin you will need to get info and links to video files/streams
+# from some web-site or online service.
+VIDEOS = {'Animals': [{'name': 'Crab',
+                       'thumb': 'http://www.vidsplay.com/vids/crab.jpg',
+                       'video': 'http://www.vidsplay.com/vids/crab.mp4',
+                       'genre': 'Animals'},
+                      {'name': 'Alligator',
+                       'thumb': 'http://www.vidsplay.com/vids/alligator.jpg',
+                       'video': 'http://www.vidsplay.com/vids/alligator.mp4',
+                       'genre': 'Animals'},
+                      {'name': 'Turtle',
+                       'thumb': 'http://www.vidsplay.com/vids/turtle.jpg',
+                       'video': 'http://www.vidsplay.com/vids/turtle.mp4',
+                       'genre': 'Animals'},
+                      {'name': 'Coot',
+                       'thumb': 'http://www.vidsplay.com/vids/turtle.jpg',
+                       'video': 'http://www.vidsplay.com/vids/turtle.mp4',
+                       'genre': 'Animals'}
+                      ],
+            'Cars': [{'name': 'Postal Truck',
+                      'thumb': 'http://www.vidsplay.com/vids/us_postal.jpg',
+                      'video': 'http://www.vidsplay.com/vids/us_postal.mp4',
+                      'genre': 'Cars'},
+                     {'name': 'Traffic',
+                      'thumb': 'http://www.vidsplay.com/vids/traffic1.jpg',
+                      'video': 'http://www.vidsplay.com/vids/traffic1.avi',
+                      'genre': 'Cars'},
+                     {'name': 'Traffic Arrows',
+                      'thumb': 'http://www.vidsplay.com/vids/traffic_arrows.jpg',
+                      'video': 'http://www.vidsplay.com/vids/traffic_arrows.mp4',
+                      'genre': 'Cars'}
+                     ],
+            'Food': [{'name': 'Chicken',
+                      'thumb': 'http://www.vidsplay.com/vids/chicken.jpg',
+                      'video': 'http://www.vidsplay.com/vids/bbqchicken.mp4',
+                      'genre': 'Food'},
+                     {'name': 'Hamburger',
+                      'thumb': 'http://www.vidsplay.com/vids/hamburger.jpg',
+                      'video': 'http://www.vidsplay.com/vids/hamburger.mp4',
+                      'genre': 'Food'},
+                     {'name': 'Pizza',
+                      'thumb': 'http://www.vidsplay.com/vids/pizza.jpg',
+                      'video': 'http://www.vidsplay.com/vids/pizza.mp4',
+                      'genre': 'Food'}
+                     ]}
+
+
+def get_categories():
+    """
+    Get the list of video categories.
+    Here you can insert some parsing code that retrieves
+    the list of video categories (e.g. 'Movies', 'TV-shows', 'Documentaries' etc.)
+    from some site or server.
+    :return: list
+    """
+    categories = []
+    for subscription in subscriptions:
+        categories.append(Channel(subscription))
+    return categories
+
+
+def get_videos(categoryName):
+    """
+    Get the list of videofiles/streams.
+    Here you can insert some parsing code that retrieves
+    the list of videostreams in a given category from some site or server.
+    :param category: str
+    :return: list
+    """
+    category = Channel(categoryName)
+    return category.videos
+
+
+def list_categories():
+    """
+    Create the list of video categories in the Kodi interface.
+    :return: None
+    """
+    # Get video categories
+    categories = get_categories()
+    # Create a list for our items.
+    listing = []
+    # Iterate through categories
+    for category in categories:
+        # Create a list item with a text label and a thumbnail image.
+        list_item = xbmcgui.ListItem(label=category.channelName, thumbnailImage=category.thumbnail)
+        # Set a fanart image for the list item.
+        # Here we use the same image as the thumbnail for simplicity's sake.
+        list_item.setProperty('fanart_image', category.thumbnail)
+        # Set additional info for the list item.
+        # Here we use a category name for both properties for for simplicity's sake.
+        # setInfo allows to set various information for an item.
+        # For available properties see the following link:
+        # http://mirrors.xbmc.org/docs/python-docs/15.x-isengard/xbmcgui.html#ListItem-setInfo
+        list_item.setInfo('video', {'title': category.channelName, 'genre': category.channelName})
+        # Create a URL for the plugin recursive callback.
+        # Example: plugin://plugin.video.example/?action=listing&category=Animals
+        url = '{0}?action=listing&category={1}'.format(_url, category.channelName)
+        # is_folder = True means that this item opens a sub-list of lower level items.
+        is_folder = True
+        # Add our item to the listing as a 3-element tuple.
+        listing.append((url, list_item, is_folder))
+    # Add our listing to Kodi.
+    # Large lists and/or slower systems benefit from adding all items at once via addDirectoryItems
+    # instead of adding one by ove via addDirectoryItem.
+    xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def list_videos(categoryName):
+    """
+    Create the list of playable videos in the Kodi interface.
+    :param category: str
+    :return: None
+    """
+    # Get the list of videos in the category.
+    videos = get_videos(categoryName)
+    # Create a list for our items.
+    listing = []
+    # Iterate through videos.
+    for video in videos:
+        # Create a list item with a text label and a thumbnail image.
+        list_item = xbmcgui.ListItem(label=video.title, thumbnailImage=video.thumbnail)
+        # Set a fanart image for the list item.
+        # Here we use the same image as the thumbnail for simplicity's sake.
+        list_item.setProperty('fanart_image', video.thumbnail)
+        # Set additional info for the list item.
+        list_item.setInfo('video', {'title': video.title, 'genre': video.title})
+        # Set additional graphics (banner, poster, landscape etc.) for the list item.
+        # Again, here we use the same image as the thumbnail for simplicity's sake.
+        list_item.setArt({'landscape': video.thumbnail})
+        # Set 'IsPlayable' property to 'true'.
+        # This is mandatory for playable items!
+        list_item.setProperty('IsPlayable', 'true')
+        # Create a URL for the plugin recursive callback.
+        # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/vids/crab.mp4
+        url = '{0}?action=play&video={1}'.format(_url, video.url)
+        # Add the list item to a virtual Kodi folder.
+        # is_folder = False means that this item won't open any sub-list.
+        is_folder = False
+        # Add our item to the listing as a 3-element tuple.
+        listing.append((url, list_item, is_folder))
+    # Add our listing to Kodi.
+    # Large lists and/or slower systems benefit from adding all items at once via addDirectoryItems
+    # instead of adding one by ove via addDirectoryItem.
+    xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_UNSORTED)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def play_video(path):
+    """
+    Play a video by the provided path.
+    :param path: str
+    :return: None
+    """
+    # Create a playable item with a path to play.
+    play_item = xbmcgui.ListItem(path=path)
+    # Pass the item to the Kodi player.
+    xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
+
+
+def router(paramstring):
+    """
+    Router function that calls other functions
+    depending on the provided paramstring
+    :param paramstring:
+    :return:
+    """
+    # Parse a URL-encoded paramstring to the dictionary of
+    # {<parameter>: <value>} elements
+    params = dict(parse_qsl(paramstring))
+    # Check the parameters passed to the plugin
+    if params:
+        if params['action'] == 'listing':
+            # Display the list of videos in a provided category.
+            list_videos(params['category'])
+        elif params['action'] == 'play':
+            # Play a video from a provided URL.
+            play_video(params['video'])
+    else:
+        # If the plugin is called from Kodi UI without any parameters,
+        # display the list of video categories
+        list_categories()
+
+
+if __name__ == '__main__':
+    # Call the router function and pass the plugin call parameters to it.
+    # We use string slicing to trim the leading '?' from the plugin call paramstring
+    router(sys.argv[2][1:])
